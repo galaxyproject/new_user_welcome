636386ba337b8478385e13653f5fefa11da6185a
Alexander OSTROVSKY <a.ostrovsky@mac.com>
2021-03-03 10:31:17 -0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-export * from "./data";
-export * from "./tools";
-export * from "./workflows";
+import { combine } from "./scripts/combine.js";
+import dataJSON from "./data/topics.json";
+import toolsJSON from "./tools/topics.json";
+import workflowsJSON from "./workflows/topics.json";
+const topicsList = [dataJSON, toolsJSON, workflowsJSON];
+
+export const newUserDict = combine(topicsList);
